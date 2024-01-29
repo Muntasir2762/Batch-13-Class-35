b210e850068d28da3057110ea24375f3160bec04
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-01-29 23:41:38 +0600

    Php Synotax & Variables

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+/*$x = 10;
+$y = 5;
+
+$z = $x+$y;
+
+echo 'The result is ='.$z;*/
+
+
+echo '<h1>Hello World</h1>';
+
+
+for($i = 0; $i<=10; $i++){
+    echo $i. '<br>';
+}
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <?php
+    echo 'Hello world';
+
+    ?> -->
+
+<span>adlkfhvladshv</span><br>
+<span>adkjfsdkljvglkdsjv</span>
+</body>
+</html>
